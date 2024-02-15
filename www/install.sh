74edadd5df0657e437ab14161ea53669c131a876
Daniel Szewczuk <daniel@szewczuk.dev>
2024-02-15 09:35:30 +0100

    bugfix

--- www/install.sh
+++ www/install.sh
@@ -1,5 +1,9 @@
 clear
-sudo su
+if [ "$(id -u)" != "0" ]; then
+    echo "This script requires root acces. Try: sudo discordo"
+    exit 1
+fi
+clear
 cd /bin
 curl https://discordo.vercel.app/discordo --output discordo
 chmod +x discordo
